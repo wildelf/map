86585fc869ed07cdd9a4e91c4a07887f52e8b367
wilde <wilde@qq.com>
2018-05-08 03:12:46 +0800

    v 1.2

--- app/tests.py
+++ app/tests.py
@@ -1,17 +1,37 @@
 from django.test import TestCase
 
 # Create your tests here.
-# import requests
-#
-# #获取外网IP
-#
-# def GetOuterIP():
-#     my_ip = requests.get('http://ip.42.pl/raw')
-#     print(type(my_ip.text))
-# if __name__ == '__main__':
-#   GetOuterIP()
+import requests
+
+#获取设备经纬值
+
+def GetOuterIP():
+    location = []
+    my_ip = requests.get('http://freegeoip.net/json/')
+    info_dict = my_ip.json()
+    print(info_dict)
+    location.append(info_dict['longitude'])
+    location.append(info_dict['latitude'])
+    return location
 
 
-with open('text.txt','r') as f:
-    for line in f:
-        print(line)
+if __name__ == '__main__':
+  location = GetOuterIP()
+  print(location)
+
+# def get_location():
+#     location = []
+#     url = 'http://api.map.baidu.com/location/ip'
+#     par = {'ak':'YN2ajqOcsFnsZI2qbsYulnL6u9iU9dhF'}
+#     my_ip_info = requests.get(url)
+#     info_dict = my_ip_info.json()
+#     print(my_ip_info)
+#     location.append(info_dict['x'])
+#     location.append(info_dict['y'])
+#     return location
+#
+#
+#     if __name__ == '__main__':
+#       location = get_location()
+#       print(location)
+
